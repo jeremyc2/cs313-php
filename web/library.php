72fca7c2c2b29afeed61c1d77524b9c14944035c
jeremyc2 <goldenmeso@gmail.com>
2018-02-06 12:45:56 -0700

    database php

---
+++ web/library.php
@@ -0,0 +1,139 @@
+<?php
+// Start the session
+session_start();
+
+    <!DOCTYPE html>
+    <html>
+    $dbUrl = getenv('ec2-54-163-228-190.compute-1.amazonaws.com');
+    
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    ?>
+    <head>
+        <title>Héroe Favorito</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Library</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <script src="funnybusiness.js"></script>
+        <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+        <script src="https://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+        <script>
+            $(function() {
+                $(".ui-widget-content").draggable({
+                    helper: "clone"
+                });
+            });
+
+        </script>
+
+
+        <link rel="stylesheet" type="text/css" href="CSS%20style%20sheet%20(2).css" />
+    </head>
+
+    <body>
+        <?php
+        require 'parts/navbar.php';
+    ?>
+            <div class="alert alert-success"> <strong>Success!</strong> One album added to cart. </div>
+            <button class="btn btn-lg" id="cart"><a href="Cart.php">View Cart</a></button>
+            <div class="well">
+                <?php
+            $count = 0;
+            foreach (scandir('images') as $file){
+                if ($file != '.') {
+                    if ($file != '..'){
+                        $slashPosition = strpos($file, "/");
+                        $dotPosition = strpos($file, ".");
+                        $artist = substr($file, $slashPosition, $dotPosition);
+                        if ($count > 2)
+                            $count = 0;
+                        if ($count == 0)
+                            echo "<div class=\"row\">";
+                        echo "<div class=\"col-sm-4\" id=\"$artist" . "col\"><img src=\"images/$file\" class=\"img-responsive ui-widget-content\" id=\"$artist\"></div>";
+                        if ($count == 2)
+                            echo "</div>";
+                        $count++;
+                    }
+                }
+            }
+        ?>
+            </div>
+
+            <script>
+                $zindex = 1000;
+
+                $.fn.exBounce = function() {
+                    var self = this;
+                    (function runEffect() {
+                        self.animate({
+                                top: 20 + "px"
+                            }, 500)
+                            .animate({
+                                top: 0 + "px"
+                            }, 500, runEffect);
+                    })();
+                    return this;
+
+                };
+
+                $(document).ready(function() {
+                    $(".ui-widget-content").height($(".ui-widget-content").width());
+                    <?php
+                    foreach($_SESSION as $key => $value){
+                        $id = "\"#" . $value . "\"";
+                        echo "$($id).exBounce();";
+                    }
+                    ?>
+                    $(".ui-widget-content").click(function() {
+                        var self = $(this);
+                        var id = self.attr('id');
+                        // If animated please stop
+                        if (self.is(':animated')) {
+                            self.stop(true, false)
+                                .css("top", "0px");
+                            $(".alert").load("manipulateCart.php", {
+                                delete: id
+                            });
+                        } else {
+                            $(".alert").load("manipulateCart.php", {
+                                add: id
+                            });
+                            self.exBounce();
+                            $(".alert").fadeIn("fast", function() {
+                                $(this).fadeOut(5000, function() {
+                                    $(this).stop(true, false);
+                                });
+                            });
+                        }
+                    });
+                    $(".ui-widget-content").mousedown(function() {
+                        $(this).css("z-index", ++$zindex);
+                    });
+                })
+
+                $(window).resize(function() {
+                    $(".ui-widget-content").height($(".ui-widget-content").width());
+                });
+
+            </script>
+
+    </body>
+
+    </html>
---
+++ web/playlist.php
@@ -0,0 +1,92 @@
+<?php
+// Start the session
+session_start();
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Héroe Favorito</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Playlists</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <script src="funnybusiness.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="CSS%20style%20sheet%20(2).css" />
+    </head>
+
+    <body>
+        <?php
+    require 'parts/navbar.php';
+        ?>
+            <div class="alert alert-warning"> <strong>Success!</strong> One album added to cart. </div>
+            <div class="well">
+                <h1> Items in your cart: </h1><br/>
+
+                <?php
+                $count = 0;
+                foreach($_SESSION as $key => $value){
+                    if ($count > 2)
+                            $count = 0;
+                        if ($count == 0)
+                            echo "<div class=\"row\">";
+                            echo "<div class=\"col-sm-4\" id=\"$value" . "col\">" . "<h3 class=\"item\" style=\"text-align:center\" id=\"$value" . "txt\">" . htmlspecialchars($value) . "</h3>" . "<img src=\"images/$value.jpg\" class=\"img-responsive ui-widget-content item\" id=\"$value\"></div>";
+                        if ($count == 2)
+                            echo "</div>";
+                        $count++;
+                }
+                if($count != 3)
+                    echo "</div>";
+                ?>
+                    <br/><br/>
+                    <p>Click on an item to delete it.</p>
+                    <br/><br/><button class="btn btn-lrg" id="back"><a href="Browse.php">Back to Browse</a></button>
+                    <button class="btn btn-lrg" id="clear">Clear All</button>
+                    <button class="btn btn-lrg" id="forward"><a href="Checkout.php">Submit Order</a></button>
+            </div>
+            <script>
+                $(document).ready(function() {
+                    $(".ui-widget-content").height($(".ui-widget-content").width());
+                    $(".item").click(function() {
+                        var id = $(this).attr('id');
+                        $(".alert").load("manipulateCart.php", {
+                            delete: id
+                        });
+                        $(this).remove();
+                        var text = "#" + id + "txt";
+                        $(text).remove();
+                        $(".alert").fadeIn("fast", function() {
+                            $(this).fadeOut(5000, function() {
+                                $(this).stop(true, false);
+                            });
+                        });
+                    });
+                    $("#clear").click(function() {
+                        $(".item").remove();
+                        $(".alert").load("manipulateCart.php", {
+                            reset: "true"
+                        });
+                        $(".alert").fadeIn("fast", function() {
+                            $(this).fadeOut(5000, function() {
+                                $(this).stop(true, false);
+                            });
+                        });
+                    });
+                });
+                $(window).resize(function() {
+                    $(".ui-widget-content").height($(".ui-widget-content").width());
+                });
+
+            </script>
+    </body>
+
+    </html>
