260cec3ea7c0fa9aaf7a7908606db8624f772927
jeremyc2 <goldenmeso@gmail.com>
2018-02-14 08:24:16 -0700

    a

--- web/send.php
+++ web/send.php
@@ -3,19 +3,19 @@
      $query = $_REQUEST['query'];
 
 
-         $dbUrl = getenv('DATABASE_URL');
-
-         $dbopts = parse_url($dbUrl);
-
-         $dbHost = $dbopts["host"];
-         $dbPort = $dbopts["port"];
-         $dbUser = $dbopts["user"];
-         $dbPassword = $dbopts["pass"];
-         $dbName = ltrim($dbopts["path"], '/');
-
-         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-         $stmt = $db->prepare(:query);
-         $stmt->bindValue(':query', $query, PDO::PARAM_STR);
-         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         // $dbUrl = getenv('DATABASE_URL');
+         //
+         // $dbopts = parse_url($dbUrl);
+         //
+         // $dbHost = $dbopts["host"];
+         // $dbPort = $dbopts["port"];
+         // $dbUser = $dbopts["user"];
+         // $dbPassword = $dbopts["pass"];
+         // $dbName = ltrim($dbopts["path"], '/');
+         //
+         // $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+         // $stmt = $db->prepare(:query);
+         // $stmt->bindValue(':query', $query, PDO::PARAM_STR);
+         // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
          echo $query . " IT WORKED!";
 ?>
