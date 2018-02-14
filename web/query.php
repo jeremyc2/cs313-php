96ad74d92c0982a4b7823ec13b9e123c69ca9716
jeremyc2 <goldenmeso@gmail.com>
2018-02-14 08:19:23 -0700

    a

--- web/query.php
+++ web/query.php
@@ -21,6 +21,15 @@ session_start();
      <head>
           <meta charset="utf-8">
           <title>tables</title>
+          <!-- Latest compiled and minified CSS -->
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+          <!-- jQuery library -->
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+          <!-- Latest compiled JavaScript -->
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
      </head>
      <body>
           <form id="formId" action="send.php" method="post">
