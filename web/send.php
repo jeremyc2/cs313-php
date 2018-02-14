672f461d30136b72aad9b3adc358f5c0cc7a689c
jeremyc2 <goldenmeso@gmail.com>
2018-02-14 10:06:21 -0700

    a

--- web/send.php
+++ web/send.php
@@ -18,13 +18,15 @@
          $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
          print_r($rows);
          echo "<table>\n";
-
+         foreach ($rows[0] as $key => $value) {
+              echo $key;
+         }
          foreach ($rows as $key => $values) // For every field name (id, name, last_name, gender)
          {
              echo "<tr>\n"; // start the row
-                 foreach ($values as $cell_key => $cell_values) // for every sub-array iterate through all values
+                 foreach ($values as $cell) // for every sub-array iterate through all values
                  {
-                    echo "\t<td>" . $cell_key . $cell_values . "</td>\n"; // write cells next to each other
+                    echo "\t<td>" . $cell . "</td>\n"; // write cells next to each other
                  }
              echo "</tr>\n"; // end row
 
