d539f7366176a4df767ae3bbb57de4da543ce1be
jeremyc2 <goldenmeso@gmail.com>
2018-02-14 14:48:54 -0700

    a

--- web/playlist.php
+++ web/playlist.php
@@ -1,7 +1,19 @@
 <?php
 // Start the session
 session_start();
-?>
+
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"], '/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    ?>
     <!DOCTYPE html>
     <html>
 
