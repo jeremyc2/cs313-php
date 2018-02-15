ef2fce94c9e8f2468628d3efd50d992805188830
jeremyc2 <goldenmeso@gmail.com>
2018-02-15 10:22:55 -0700

    a

--- web/courses.php
+++ web/courses.php
@@ -1,4 +1,15 @@
 <?php
+     $dbUrl = getenv('DATABASE_URL');
+
+     $dbopts = parse_url($dbUrl);
+
+     $dbHost = $dbopts["host"];
+     $dbPort = $dbopts["port"];
+     $dbUser = $dbopts["user"];
+     $dbPassword = $dbopts["pass"];
+     $dbName = ltrim($dbopts["path"],'/');
+
+     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 ?>
      <!DOCTYPE html>
      <html>
@@ -8,6 +19,8 @@
           </head>
           <body>
                <h1>Courses</h1>
-               
+               <?php
+                    
+               ?>
           </body>
      </html>
