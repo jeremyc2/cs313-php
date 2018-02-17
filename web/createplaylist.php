cbf452bf594de8fa21333f31a48cd751ef28303c
Jeremy Ryan Chandler <jeremy@MacBook-Pro-de-Jeremy.local>
2018-02-17 14:04:58 -0700

    a

--- web/createplaylist.php
+++ web/createplaylist.php
@@ -20,8 +20,23 @@ session_start();
       <head>
         <meta charset="utf-8">
         <title>Create Playlist</title>
-      </head>
-      <body>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <script src="funnybusiness.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="CSS%20style%20sheet%20(2).css" />
+    </head>
+
+    <body>
+        <?php
+    require 'parts/navbar.php';
+        ?>
         <form class="" action="" method="post">
           <label for="">Pick a playlist:</label><br>
           <select class="" name="playlist">
