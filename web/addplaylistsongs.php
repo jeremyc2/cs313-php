c29ee52aa367a0b01c87efe5f960f64e2cd8b033
Jeremy Ryan Chandler <jeremy@MacBook-Pro-de-Jeremy.local>
2018-02-17 14:47:20 -0700

    a

--- web/addplaylistsongs.php
+++ web/addplaylistsongs.php
@@ -14,10 +14,10 @@ session_start();
 
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-    $stmt = $db->prepare('INSERT INTO songs_playlists');
-    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-    $stmt->execute();
+    // $stmt = $db->prepare('INSERT INTO songs_playlists');
+    // $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    // $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    // $stmt->execute();
 
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
               echo "<br><br><br><h1>Playlists</h1><br>";
