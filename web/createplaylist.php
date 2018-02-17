ffe96594cbf2d2909c346d4a89aa4b234f864706
Jeremy Ryan Chandler <jeremy@MacBook-Pro-de-Jeremy.local>
2018-02-17 13:28:13 -0700

    a

--- web/createplaylist.php
+++ web/createplaylist.php
@@ -15,4 +15,27 @@ session_start();
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     ?>
 
-    
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Create Playlist</title>
+      </head>
+      <body>
+        <form class="" action="" method="post">
+          <label for="">Pick a playlist:</label><br>
+          <select class="" name="playlist">
+            <?php
+              $query = "select title, id from playlists;";
+              $stmt = $db->prepare($query);
+              $stmt->execute();
+              $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+              foreach ($rows as $row) {
+                echo "<option value="$row['id']">$row['title']</option>";
+              }
+             ?>
+          </select>
+        </form>
+
+      </body>
+    </html>
