c7646bd86a59e80260c233b5a2dc3c3a85850ff3
Jeremy Ryan Chandler <jeremy@MacBook-Pro-de-Jeremy.local>
2018-02-20 10:56:28 -0700

    a

--- web/Homehash.php
+++ web/Homehash.php
@@ -6,7 +6,13 @@
       <title>Home</title>
     </head>
     <body>
-      
+      <form class="" action="" method="post">
+        <label for="">Enter your username:</label><br>
+        <input type="text" name="username" value=""><br>
+        <label for="password">Enter your password:</label><br>
+        <input type="password" name="password" value=""><br>
+        <button type="submit" name="button"></button>
+      </form>
 
     </body>
   </html>
