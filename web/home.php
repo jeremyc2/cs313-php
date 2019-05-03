4e50313c63af2d28dbbe4ebbecd723d1d3c70b74
jeremyc2 <goldenmeso@gmail.com>
2019-05-02 19:18:07 -0600

    Update home.php

--- web/home.php
+++ web/home.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         <title>Héroe Favorito</title>
         <meta charset="utf-8"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Latest compiled and minified CSS -->
+         Latest compiled and minified CSS 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="stylesheet.css">
 
-        <!-- jQuery library -->
+         jQuery library 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-        <!-- Latest compiled JavaScript -->
+         Latest compiled JavaScript 
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         
         <script src="funnybusiness.js"></script>
@@ -31,6 +31,33 @@
             <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Ktq4zATPFsI?start=60" allowfullscreen></iframe>
         </div>
     </div>
+
+</body>
+</html> 
+-->
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>The XMLHttpRequest Object</h1>
+
+<button type="button" onclick="loadDoc()">Request data</button>
+
+<p id="demo"></p>
+ 
+<script>
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "http://40.114.119.178:8000/v1/invoke", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("module=/add-note.xqy");
+}
+</script>
 
 </body>
 </html>
